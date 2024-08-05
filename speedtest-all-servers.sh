4e82cc504dde6060ca244fb15adfba4adc852cbb
Jarukrit <jarukritsripaploen@protonmail.com>
2024-08-05 20:19:11 +0700

    Feat: Speedtest CSV Output

--- speedtest-all-servers.sh
+++ speedtest-all-servers.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 SERVER_ID_LIST=$(speedtest-cli --list | grep -oE '^[0-9]+')
 
+speedtest-cli --csv-header &> speedtest_2.csv
 for id in $SERVER_ID_LIST; do
-    echo "Speedtesting Server ID: $id"
+    timestamp="$(date +"%T")"
+    echo "Initiating test with server ID: $id, $(date +%c)"
+    speedtest-cli --csv --server $id &>> speedtest_2.csv
+    echo "Done"
 done
